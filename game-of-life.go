bfc2c7ab9c7befee58b6ec6e305333599455a7da
Taylor Caswell <tcaswell7@gmail.com>
2016-12-08 01:04:36 -0800

    Implemented most of the program. Need to fix bug with how the grid is displayed

--- game-of-life.go
+++ game-of-life.go
@@ -1,20 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 const numColumns, numRows = 10, 10
 const dead, alive = false, true
 
 var grid [][]bool
+var changedCells []cellCoordinates
+
+type cellCoordinates struct {
+	x int
+	y int
+}
 
 func main() {
 	initializeGrid()
 
+	grid[1][2] = true
 	grid[5][5] = true
-	grid[4][4] = true
-	grid[6][6] = true
+	grid[6][5] = true
 
 	displayGrid()
+	time.Sleep(0)
+	/*
+		// Game loop
+		for {
+			time.Sleep(1000 * time.Millisecond)
+			tick()
+			for _, cell := range changedCells {
+				grid[cell.x][cell.y] = !grid[cell.x][cell.y]
+			}
+			displayGrid()
+		}
+	*/
 }
 
 func initializeGrid() {
@@ -31,14 +52,49 @@ func initializeGrid() {
 }
 
 func displayGrid() {
+	fmt.Print("\033[2J")
 	for _, columns := range grid {
 		for _, cellState := range columns {
 			if cellState {
 				fmt.Print("*")
 			} else {
-				fmt.Print(" ")
+				fmt.Print("-")
 			}
 		}
 		fmt.Println()
 	}
+}
+
+func tick() {
+	changedCells = nil
+
+	for x, columns := range grid {
+		for y, cellState := range columns {
+			if cellState != willBeAlive(x, y) {
+				changedCells = append(changedCells, cellCoordinates{x, y})
+			}
+		}
+	}
+}
+
+func willBeAlive(x int, y int) bool {
+	numLiveNeighbors := getNumLiveNeighbors(x, y)
+	fmt.Printf("(%d, %d, %d)", x, y, numLiveNeighbors)
+	return !(numLiveNeighbors < 2 || numLiveNeighbors > 3)
+}
+
+func getNumLiveNeighbors(x int, y int) int {
+	neighbors := 0
+	for xx := x - 1; xx <= x+1; xx = xx + 1 {
+		for yy := y - 1; yy <= y+1; yy = yy + 1 {
+			if xx != x && yy != y && inBounds(xx, yy) && grid[xx][yy] {
+				neighbors = neighbors + 1
+			}
+		}
+	}
+	return neighbors
+}
+
+func inBounds(x int, y int) bool {
+	return x >= 0 && x < numColumns && y >= 0 && y < numRows
 }
